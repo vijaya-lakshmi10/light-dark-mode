5f7f6daa3b86d381066e77bb3f0aba66bddc33cd
vijaya-lakshmi10 <135212394+vijaya-lakshmi10@users.noreply.github.com>
2023-07-10 12:49:56 +0530

    Update index.js

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -21,12 +21,12 @@ class LightDarkMode extends Component{
             <div className="light-dark-mode-container">
                     {changeModeBtn ? ( <div className="light-dark-mode-sub-container1">
                     <h1 className="heading1">Click To Change Mode</h1>
-                    <button className="light-dark-btn" type="button" onClick={this.changeModeBtnFunc}>Light Mode</button>
+                    <button className="light-btn" type="button" onClick={this.changeModeBtnFunc}>Light Mode</button>
                     </div>)
                     : 
                     (<div className="light-dark-mode-sub-container2">
                     <h1 className="heading2">Click To Change Mode</h1>
-                    <button className="light-dark-btn" type="button" onClick={this.changeModeBtnFunc}>Dark Mode</button>
+                    <button className="dark-btn" type="button" onClick={this.changeModeBtnFunc}>Dark Mode</button>
                     </div>
                     )}
             </div>
